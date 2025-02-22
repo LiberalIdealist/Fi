ac595e83037d1d4bd6bf4381513ca7b9fd464c3f
LiberalIdealist <liberalidealist@gmail.com>
2025-02-22 16:45:39 +0000

    Connected frontend to backend

--- fi-frontend/next.config.ts
+++ fi-frontend/next.config.ts
@@ -1,7 +1,5 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+module.exports = {
+  env: {
+    BACKEND_URL: "https://wealthme.duckdns.org",
+  },
 };
-
-export default nextConfig;
