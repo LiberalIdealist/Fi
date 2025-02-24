01e9820c701659d6c3ece88a15f32c1e1a7a0bce
LiberalIdealist <liberalidealist@gmail.com>
2025-02-24 00:36:25 +0000

    Remove unused assets and update configuration for improved performance

--- fi-frontend/next.config.ts
+++ fi-frontend/next.config.ts
@@ -1,5 +1,18 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   env: {
     BACKEND_URL: "https://wealthme.duckdns.org",
   },
+  images: {
+    domains: ['lh3.googleusercontent.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        pathname: '/a/**',
+      },
+    ],
+  },
 };
+
+module.exports = nextConfig;
